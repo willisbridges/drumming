5b5198a30fe454301daeb78e3b6934940fb0918b
Willis Bridges <willisdbridges@gmail.com>
2023-10-29 12:27:04 -0700

    framework for tkinter gui

---
+++ tk_gui/drum_tab_interface.py
@@ -0,0 +1,14 @@
+import _tkinter as tk
+
+
+def display_drum_tab():
+    root = tk.Tk()
+    root.title("drum time mfer")
+
+    # TODO: create and configure widgets
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    display_drum_tab()
